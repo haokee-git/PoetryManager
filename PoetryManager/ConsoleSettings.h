aab67c790317fa03fee6d0fa34c3e8b1d59ad9f9
haokee-git <146501686+haokee-git@users.noreply.github.com>
2024-02-20 18:15:48 +0800

    1.1.9v 更新
    ## bug fix.
    
    更改了输入方式，避免了键入 EOF（Ctrl+Z）无法继续正常读入（更改为了 Ctrl+E）。修复了打印的时候留有子符的问题。
    
    ## 优化
    
    优化了打印方式，添加了居中功能。单键更改为了组合键，方便在中文模式下操作而不用切换中英文。

--- PoetryManager/ConsoleSettings.h
+++ PoetryManager/ConsoleSettings.h
@@ -2,16 +2,17 @@
 
 #include <Windows.h>
 
-#define UP    72
-#define DOWN  80
-#define ENTER 13
-#define ESC   27
-#define DEL   83
-#define A     97
-#define D     100
-#define U     117
-#define H     104
-#define Q     113
+#define UP     72
+#define DOWN   80
+#define ENTER  13
+#define ESC    27
+#define DEL    83
+#define CTRL_D 4
+#define CTRL_E 5
+#define CTRL_H 8
+#define CTRL_N 14
+#define CTRL_U 21
+#define CTRL_Q 17
 
 void setPos(int y, int x) {
   COORD coord;
--- PoetryManager/PoetryManager.cpp
+++ PoetryManager/PoetryManager.cpp
@@ -8,6 +8,7 @@ using namespace std;
 
 string appData;
 vector<string> fileList;
+int length;
 
 void init() {
   hideCursor();
@@ -18,12 +19,16 @@ void addFile() {
   showCursor();
   system("cls");
   cout << "[添加古诗] 格式：标题/作者/正文（最少三行）" << endl;
-  cout << "输入完成后按 Ctrl+Z 并换行" << endl;
+  cout << "输入完成后按 Ctrl + E 并换行" << endl;
   string title, author, buffer;
   vector<string> body;
   getline(cin, title);
   getline(cin, author);
-  while (getline(cin, buffer)) {
+  while (1) {
+    getline(cin, buffer);
+    if (buffer[0] == CTRL_E) {
+      break;
+    }
     body.push_back(buffer);
   }
   ofstream fout(string("./AppData/") + title);
@@ -37,12 +42,27 @@ void addFile() {
 
 void preview(string title) {
   setPos(4 + fileList.size() + 1, 1);
+  for (int i = 1; i <= length; i++) {
+    cout << "\33[2K\r\n";
+  }
+  length = 0;
+  setPos(4 + fileList.size() + 1, 1);
   ifstream fin(string("./AppData/") + title);
   string buffer;
+  vector<string> body;
+  int maxLength = 0;
   while (getline(fin, buffer)) {
-    cout << "\33[2K\r" << buffer << '\n';
+    body.push_back(buffer);
+    maxLength = max(maxLength, buffer.size());
+    length++;
   }
-  auto key = _getch();
+  for (string line : body) {
+    for (int i = 1; i <= maxLength / 2 - (line.size() - 1) / 2; i++) {
+      cout << ' ';
+    }
+    cout << line << '\n';
+  }
+  fin.close();
 }
 
 void update(string title) {
@@ -53,7 +73,11 @@ void update(string title) {
   string author, buffer;
   vector<string> body;
   getline(cin, author);
-  while (getline(cin, buffer)) {
+  while (1) {
+    getline(cin, buffer);
+    if (buffer[0] == CTRL_E) {
+      break;
+    }
     body.push_back(buffer);
   }
   ofstream fout(string("./AppData/") + title);
@@ -62,7 +86,7 @@ void update(string title) {
   for (auto line : body) {
     fout << line << '\n';
   }
-  fout.close();
+  fout.close();\
 }
 
 void del(string title) {
@@ -73,21 +97,22 @@ void help() {
   system("cls");
   cout << R"(== 帮助 ==
 
-本软件由好渴鹅制作，当前版本为 1.1.5v。
+本软件由好渴鹅制作，当前版本为 1.1.9v。
 
 按键说明：
 
   ↑ 键：把当前选择的古诗词光标向上调整
   ↓ 键：把当前选择的古诗词光标向下调整
-  A 键：添加新的古诗词
-  U 键：更改已有的（当前光标）古诗词
-  D 键：删除当前光标上的古诗词
-  Q 键：退出当前程序
+  CTRL + N 键：添加新的古诗词
+  CTRL + U 键：更改已有的（当前光标）古诗词
+  CTRL + D 或 Delete 键：删除当前光标上的古诗词
+  CTRL + Q 键：退出当前程序
 
 输入说明：
 
-  在添加新的古诗词的时候，请在第一行输入你想要添加的古诗词的标题，然后在第二行输入你想要添加的古诗词的作者、朝代等信息，接下来若干行都是正文的内容，直到输入完成就在单独的一行当中按下 Ctrl+Z 并回车则完成古诗词的添加。
+  在添加新的古诗词的时候，请在第一行输入你想要添加的古诗词的标题，然后在第二行输入你想要添加的古诗词的作者、朝代等信息，接下来若干行都是正文的内容，直到输入完成就在单独的一行当中按下 Ctrl+E 并回车则完成古诗词的添加。
   更改已有的古诗词的时候，不需要重新输入古诗词的标题了，只需要输入朝代和正文并以同样的方式结束输入即可。
+  无需自行排版，程序会默认居中。
 
 若了解完本帮助，请按任意键退出。
 )";
@@ -101,8 +126,8 @@ void quit() {
 
 void loop() {
   init();
-  cout << "古诗词管理器 [版本 1.1.5v]" << endl;
-  cout << "好渴鹅制作，如需帮助请按 H。" << endl;
+  cout << "古诗词管理器 [版本 1.1.9v]" << endl;
+  cout << "好渴鹅制作，如需帮助请按 CTRL + H。" << endl;
   cout << endl;
   while (1) {
     int pt = 0;
@@ -125,19 +150,22 @@ void loop() {
       case DOWN:
         pt = min(pt + 1, fileList.size() - 1);
         break;
-      case A:
+      case CTRL_N:
         addFile();
         return;
-      case U:
+      case CTRL_U:
         update(fileList[pt]);
         return;
-      case D:
+      case CTRL_D:
         del(fileList[pt]);
         return;
-      case H:
+      case DEL:
+        del(fileList[pt]);
+        return;
+      case CTRL_H:
         help();
         return;
-      case Q:
+      case CTRL_Q:
         quit();
       }
     }
